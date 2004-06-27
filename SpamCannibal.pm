222b49d5a24dc94f956497ccf46f7368a6531e76
Michael Robinton <michael@bizsystems.com>
2004-06-27 21:07:40 +0000

    Import of MIKER/Mail-SpamCannibal-0.42 from CPAN.
    gitpan-cpan-distribution: Mail-SpamCannibal
    gitpan-cpan-version:      0.42
    gitpan-cpan-path:         MIKER/Mail-SpamCannibal-0.42.tar.gz
    gitpan-cpan-author:       MIKER
    gitpan-cpan-maturity:     released

--- SpamCannibal.pm
+++ SpamCannibal.pm
@@ -5,7 +5,7 @@ use strict;
 #use diagnostics;
 use vars qw($VERSION);
 
-$VERSION = do { my @r = (q$Revision: 0.41 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+$VERSION = do { my @r = (q$Revision: 0.42 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
 
 sub DESTROY {};
 
